afb75a03fb45e2b3ff84313d0b8e8ff89c1607f5
Tom BUTIN <tom.frefre.13@gmail.com>
2025-12-07 16:47:27 +0100

    chore: reorganize project and add makefile

--- Code/ConwayRule.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "IRule.hpp"
-#include "Grid.hpp"
-
-class ConwayRule : public IRule {
-public:
-    std::unique_ptr<CellState> calculerEtat(const Cell& cell, const Grid& grid) const override;
-};
